381e8d236390d064a90f154001ddc06bf5908b5a
soyoybean <sok020@ucsd.edu>
2020-01-22 00:21:23 -0800

    Initial Commit

---
+++ tipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalc
+//
+//  Created by Soyon Kim on 1/22/20.
+//  Copyright © 2020 Soyon Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
